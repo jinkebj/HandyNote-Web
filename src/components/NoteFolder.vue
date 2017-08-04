226a3c073af0e293fa09c2e57d49ad0a7ff7db97
Jin Ke <jinkebj@126.com>
2017-08-05 00:23:00 +0800

    implement prepareFolderData function

--- src/components/NoteFolder.vue
+++ src/components/NoteFolder.vue
@@ -209,11 +209,48 @@ export default {
 
     prepareFolderData (inputData) {
       if (inputData.length === 0) return
-      let outputData = {}
-      outputData.id = inputData[0]._id
-      outputData.label = inputData[0].name
-      outputData.ancestor_ids = inputData[0].ancestor_ids
-      this.noteFolders[0].children.push(outputData)
+
+      let rootItem = this.noteFolders[0]
+      rootItem.children = []
+
+      let itemMap = new Map()
+      itemMap.set('mytest-Root', rootItem)
+
+      let maxLevel = 0
+      let levelMap = new Map()
+
+      inputData.forEach(item => {
+        const curLevel = item.ancestor_ids.length
+        maxLevel = Math.max(curLevel, maxLevel)
+        itemMap.set(item._id, {
+          id: item._id,
+          label: item.name,
+          ancestor_ids: item.ancestor_ids,
+          children: []
+        })
+        let levelItem = {itemId: item._id, parentId: item.parent_id}
+        if (levelMap.has(curLevel)) {
+          levelMap.get(curLevel).push(levelItem)
+        } else {
+          levelMap.set(curLevel, [levelItem])
+        }
+      })
+      console.log(itemMap)
+      console.log('maxLevel is: ' + maxLevel)
+      console.log(levelMap)
+
+      for (let i = maxLevel; i > 0; i--) {
+        if (!levelMap.has(i)) continue
+        let levelItems = levelMap.get(i)
+        levelItems.forEach(item => {
+          if (itemMap.has(item.parentId) && itemMap.has(item.itemId)) {
+            itemMap.get(item.parentId).children.push(itemMap.get(item.itemId))
+          }
+        })
+      }
+      console.log(itemMap.get('mytest-Root'))
+
+      this.noteFolders = [itemMap.get('mytest-Root')]
     },
 
     addFolder (store, data) {
