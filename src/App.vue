3d8d1044efe45c77ea93467207873cccdfd81f1b
Jin Ke <jinkebj@126.com>
2018-06-25 17:06:48 +0800

    style scrollbar for chrome

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,26 @@ html, body {
   padding: 0;
   font-family: sans-serif;
 }
+
+/*  style scrollbar for chrome */
+::-webkit-scrollbar {
+  width: 8px;
+  background-color: rgba(0, 0, 0, 0, 0.09);
+}
+
+::-webkit-scrollbar:hover {
+  border-left: 1px solid #ddd;
+}
+
+::-webkit-scrollbar-thumb:vertical {
+  background: rgba(0, 0, 0, 0.2);
+  -webkit-border-radius: 100px;
+}
+
+::-webkit-scrollbar-thumb:vertical:hover {
+  background: rgba(0, 0, 0, 0.35);
+  -webkit-border-radius: 100px;
+}
 </style>
 
 <script>
